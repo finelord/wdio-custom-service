1540c6293eb6b20e3c920db938479ad6efe28dc0
finelord <finelord@gmail.com>
2019-02-14 21:46:59 -0500

    working test example

---
+++ wdio.conf.js
@@ -0,0 +1,25 @@
+exports.config = {
+    runner: 'local',
+    specs: [
+        './specs/*.spec.js'
+    ],
+    maxInstances: 10,
+    capabilities: [{
+        maxInstances: 5,
+        browserName: 'chrome'
+    }],
+    logLevel: 'info',
+    deprecationWarnings: true,
+    bail: 0,
+    waitforTimeout: 10000,
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3,
+    services: ['selenium-standalone'],
+    framework: 'jasmine',
+    reporters: ['dot','spec'],
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 60000,
+        expectationResultHandler: function(passed, assertion) {
+        }
+    }
+}
